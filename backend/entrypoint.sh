2508d100d0a821844949a5e311ecb676fe3b00b6
giuseppe.lapietra <g.lapietra@gdservices.tech>
2025-12-31 19:10:21 +0100

    feat: prod

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,4 +8,10 @@ if [ "$DEBUG" = "False" ]; then
     echo "PostgreSQL started"
 fi
 
-exec uvicorn backend.main:app --host 0.0.0.0 --port 8000 --reload
+if [ "$DEBUG" = "True" ]; then
+    echo "Running in DEBUG mode"
+    exec uvicorn backend.main:app --host 0.0.0.0 --port 8000 --reload
+else
+    echo "Running in PRODUCTION mode"
+    exec uvicorn backend.main:app --host 0.0.0.0 --port 8000
+fi
